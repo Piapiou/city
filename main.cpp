ac795f88fd6387d8236dbd845eba81cc6396bff0
Robin <robinboitemail@gmail.com>
2016-12-08 16:22:35 +0100

    QuadArea

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <mesh.h>
+#include <quadarea.h>
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
+    srand(time(NULL));
+    Mesh m=Mesh();
+    QuadArea q=QuadArea(Point(0,0,0),Point(1000,0,0),Point(1000,1000,0),Point(0,1000,0));
+    q.subdivision(m);
+    m.toOBJ("test.obj");
     return app.exec();
 }
--- quadarea.cpp
+++ quadarea.cpp
@@ -2,7 +2,7 @@
 
 QuadArea::QuadArea()
 {
-    srand(time(NULL));
+
 }
 
 QuadArea::QuadArea(Point pa, Point pb, Point pc, Point pd)
@@ -12,30 +12,34 @@ QuadArea::QuadArea(Point pa, Point pb, Point pc, Point pd)
     this->p[2]=pc;
     this->p[3]=pd;
 
-    srand(time(NULL));
+
 }
 
 void QuadArea::subdivision(Mesh& m)
 {
-    int seuil=100;
+    int seuil=1600;
 
-    if (computeQuadArea(p) < seuil){
-        printf("%f",this->p[0].x());
+    if (computeQuadArea(p[0],p[1],p[2],p[3]) < seuil){
+        Floor f=Floor(p[0],p[1],p[2],p[3],2);
+        f.subdivision(m);
         return;
     }
 
-    //int alea=rand()%100;
-    int alea=1;
+    int alea=rand()%100;
     if(alea<=(int)QGram::QQ){
-        Point m1=randomMid(p[longestEdge()],p[(longestEdge()+2)%4]);
-        Point m2=randomMid(p[(longestEdge()+1)%4],p[(longestEdge()+3)%4]);
-        QuadArea q1= QuadArea(p[longestEdge()],m1,m2,p[(longestEdge()+2)%4]);
+        Point m1=randomMid(p[longestEdge()],p[(longestEdge()+1)%4]);
+        Point m2=randomMid(p[(longestEdge()+2)%4],p[(longestEdge()+3)%4]);
+        QuadArea q1= QuadArea(p[longestEdge()],m1,m2,p[(longestEdge()+3)%4]);
         QuadArea q2= QuadArea(m1,p[(longestEdge()+1)%4],p[(longestEdge()+2)%4],m2);
         q1.subdivision(m);
         q2.subdivision(m);
     }
     else if(alea<=(int)QGram::QTT){
+        TriangleArea t1=TriangleArea(p[0],p[1],p[2]);
+        TriangleArea t2=TriangleArea(p[2],p[3],p[0]);
 
+        t1.subdivision(m);
+        t2.subdivision(m);
     }
 }
 
