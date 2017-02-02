a02a6a851cacfb456f782d9533ceb0f61dcb42c7
Exospector <pcheminel@gmail.com>
2017-02-02 12:14:53 +0100

    Update Quartiers

--- groundfloor.h
+++ groundfloor.h
@@ -11,6 +11,10 @@ public:
     void subdivision(Mesh& m);
     void makeMesh(Mesh &m);
 
+    static constexpr float FLOOR_T1 = 0.85;
+    static constexpr float FLOOR_T2 = 0.99;
+    static constexpr float FLOOR_T3 = 1.00;
+
 private:
     Point p1, p2, p3, p4;
     float size;
--- roof.cpp
+++ roof.cpp
@@ -1,4 +1,5 @@
 #include "roof.h"
+#include "function.h"
 
 Roof::Roof(Point p1, Point p2, Point p3, Point p4)
 {
@@ -10,6 +11,25 @@ Roof::Roof(Point p1, Point p2, Point p3, Point p4)
 
 void Roof::makeMesh(Mesh& m) {
 
-    m.merge(Mesh::makeRoof(p1,p2,p3,p4));
+    if (!(distanceTwoPoints(p1, p2) < 2
+          || distanceTwoPoints(p2, p3) < 2
+          || distanceTwoPoints(p3, p4) < 2
+          || distanceTwoPoints(p4, p1) < 2
+          || distanceTwoPoints(p1, p3) < 2
+          || distanceTwoPoints(p2, p4) < 2)) {
+        Point p[4];
+        p[0] = p1;
+        p[1] = p2;
+        p[2] = p3;
+        p[3] = p4;
+
+        shrinkN(p,4,1);
+
+        p1 = p[0];
+        p2 = p[1];
+        p3 = p[2];
+        p4 = p[3];
+    }
+    m.merge(Mesh::makeFloor(p1,p2,p3,p4, 3));
 
 }
